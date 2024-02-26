05e145aa928ba46e825e1efd94125ea5d8e43189
n-ratinac <nikola.ratinac@prvagimnazija.edu.rs>
2024-02-26 17:53:28 +0100

    add aditional tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -38,3 +38,37 @@ def test_factorize_number(client):
     res = client.get("/factorize", query_string={"number": 75})
     assert res.status_code == 200
     assert res.json == [3, 5, 5]
+
+def test_factorize_number_without_number(client):
+    res = client.get("/factorize")
+    assert res.status_code == 400
+    assert res.json == {"message": "Missing parameter: number"}
+
+def test_number_of_digits(client):
+    res = client.get("/number-of-digits", query_string={"number": 75})
+    assert res.status_code == 200
+    assert int(res.data) == 2
+
+def test_number_of_digits_without_number(client):
+    res = client.get("/number-of-digits")
+    assert res.status_code == 400
+    assert res.json == {"message" : "Missing parameter: number"}
+
+def test_post_question(client):
+    question = {
+        "question": "What is the answer to life, the universe, and everything?",
+        "answer": 42,
+        "dummies": ["Family", "Love", "Health"]
+    }
+
+    res = client.post("/question", json=question)
+    assert res.status_code == 201
+    
+def test_get_question(client):
+    res = client.get("/question")
+    assert res.status_code == 200
+    assert len(res.json) > 0
+
+def test_get_question_by_id(client):
+    res = client.get("/question", query_string={"id": "1"})
+    assert res.status_code == 200
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -3,4 +3,8 @@ import src.functions as f
 def test_factorize():
     assert f.factorize(75) == [3, 5, 5]
     assert f.factorize(1) == []
-    
+
+def test_number_of_digits():
+    assert f.number_of_digits(75) == 2
+    assert f.number_of_digits(1) == 1
+    assert f.number_of_digits(0) == 0
