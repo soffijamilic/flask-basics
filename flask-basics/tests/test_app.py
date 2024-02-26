819baddda5619f9e6ef37dbdc0d96025d5a23750
n-ratinac <nikola.ratinac@prvagimnazija.edu.rs>
2024-02-26 12:42:35 +0100

    setup project

---
+++ flask-basics/tests/test_app.py
@@ -0,0 +1,30 @@
+import pytest
+from src.app import create_app
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+def test_request_example(client):
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Hello, World!" in response.data
