c4f0fe0d57e1d785bf476e3aa0dcf2d84ae459c1
n-ratinac <nikola.ratinac@prvagimnazija.edu.rs>
2024-03-03 18:20:12 +0100

    solve get requests

--- src/app.py
+++ src/app.py
@@ -1,18 +1,20 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
+
+import src.functions as f
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
+        SECRET_KEY="dev",
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
@@ -24,8 +26,29 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/')
+    @app.route("/")
     def hello():
-        return 'Hello, World!'
+        return "Hello, World!"
+
+    @app.route("/count-words", methods=["GET"])
+    def count_words():
+        sentence = request.args.get("sentence")
+        if sentence is None:
+            return b"0"
+        return str(len(sentence.split()))
+
+    @app.route("/factorize", methods=["GET"])
+    def factorize():
+        number = request.args.get("number")
+        if number is None:
+            return {"message": "Missing parameter: number"}, 400
+        return f.factorize(int(number))
+
+    @app.route("/number-of-digits", methods=["GET"])
+    def number_of_digits():
+        number = request.args.get("number")
+        if number is None:
+            return {"message": "Missing parameter: number"}, 400
+        return str(f.number_of_digits(int(number)))
 
     return app
